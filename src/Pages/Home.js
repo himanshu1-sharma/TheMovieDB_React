b4c93663e7f4093faed8464f0a5689652aeac216
Himanshu <sharmagi1998@gmail.com>
2023-03-27 12:53:33 +0530

    changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import HomeContent from './HomeContent/HomeContent'
+import { Button } from 'antd';
+import { DatePicker } from 'antd';
+import { SkinOutlined  } from '@ant-design/icons';
+import { Col, Row } from 'antd';
 
 const Home = () => {
     return (
         <>
             <HomeContent />
             <h1>Home</h1>
+            <Button type="primary">Button</Button>
+            <DatePicker />
+            <SkinOutlined />
+
         </>
     )
 }

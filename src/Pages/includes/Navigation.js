4a84b29e7abf657975e2dc91983710070700a8ca
Himanshu <sharmagi1998@gmail.com>
2023-03-30 18:04:53 +0530

    add navigation

--- src/Pages/includes/Navigation.js
+++ src/Pages/includes/Navigation.js
@@ -8,7 +8,33 @@ import './Navigation.css'
 const Navigation = () => {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavLink exact activeClassName="active" to='/'>
+                                <li>Home</li>
+                            </NavLink>
+                            <NavLink exact activeClassName="active" to='/popular'>
+                                <li>Popular</li>
+                            </NavLink>
+                            <NavLink exact activeClassName="active" to='/top-rated'>
+                                <li>Top Rated</li>
+                            </NavLink>
+                            <NavLink exact activeClassName="active" to='/upcoming'>
+                                <li>Upcoming</li>
+                            </NavLink>
+                            <NavLink exact activeClassName="active" to='/users'>
+                                <li>Users</li>
+                            </NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
+            {/* <Navbar bg="dark" variant="dark">
                 <Container>
                     <Nav className="me-auto">
                         <ul>
@@ -30,7 +56,7 @@ const Navigation = () => {
                         </ul>
                     </Nav>
                 </Container>
-            </Navbar>
+            </Navbar> */}
         </>
     )
 }
